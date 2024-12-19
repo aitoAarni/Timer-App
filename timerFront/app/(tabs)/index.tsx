317293bf6bbc89bff2966ca6c59a5b90cdeb0a67
aitoAarni <aarni.rechardt@gmail.com>
2024-12-19 19:54:39 +0200

    Test expo routes

---
+++ timerFront/app/(tabs)/index.tsx
@@ -0,0 +1,5 @@
+import { Link } from 'expo-router'
+
+export default function Index() {
+    return <Link href="/settings"> TO INDEEEX</Link>
+}
---
+++ timerFront/app/(tabs)/settings.tsx
@@ -0,0 +1,5 @@
+import { Link } from 'expo-router'
+
+export default function Settings() {
+    return <Link href="/">TO SETTIIIINGS</Link>
+}
