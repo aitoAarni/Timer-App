068b2d3d904c5bdae361d7a262a87aa16c03957c
aitoAarni <aarni.rechardt@gmail.com>
2024-12-19 21:12:33 +0200

    fix homeViewv and settingsViewv

--- timerFront/app/index.tsx
+++ timerFront/app/index.tsx
@@ -1,9 +1,11 @@
+import { Link } from 'expo-router'
 import { Text, View } from 'react-native'
 
 export default function HomeView() {
     return (
         <View>
             <Text>HOME SCREEN IS HERE</Text>
+            <Link href={'/settings'}>settings</Link>
         </View>
     )
 }
