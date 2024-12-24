d3bb6d92e29bd385065f51b9651faa29a49ebd3c
aitoAarni <aarni.rechardt@gmail.com>
2024-12-24 04:46:28 +0200

    fix custom fonts work now

--- timerFront/components/Text.tsx
+++ timerFront/components/Text.tsx
@@ -1,4 +1,8 @@
 import theme from '@/theme'
+import { useFonts } from 'expo-font'
+import { getLoadedFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
 import {
     Text as NativeText,
     StyleProp,
@@ -13,18 +17,35 @@ interface Props extends TextProps {
     style?: StyleProp<TextStyle>
 }
 
-const Text = function ({ color, style, ...props }: Props) {
+SplashScreen.preventAutoHideAsync()
+
+const Text = function ({ style, color, fontSize, ...props }: Props) {
+    const [loaded, error] = useFonts({
+        'PermanentMarker-Regular': require('@/assets/fonts/PermanentMarker-Regular.ttf'),
+    })
+    useEffect(() => {
+        if (loaded || error) {
+            SplashScreen.hideAsync()
+        }
+    }, [loaded, error])
+
+    if (!loaded && !error) {
+        return null
+    }
+
+    console.log('loaded fonts: ', getLoadedFonts())
     const textStyle: StyleProp<TextStyle>[] = [
         styles.text,
         style,
-        color && { color },
+        color ? { color } : null,
+        fontSize ? { fontSize } : null,
     ]
     return <NativeText style={textStyle} {...props} />
 }
 
 const styles = StyleSheet.create({
     text: {
-        fontFamily: 'PlaywriteFont',
+        fontFamily: 'PermanentMarker-Regular',
         color: 'red',
         fontSize: theme.fontSizes.timer,
     },
