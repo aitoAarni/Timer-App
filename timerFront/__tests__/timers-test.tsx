e2b248d5affac3ed5b86ffc4c3feff7ec0d753d4
aitoAarni <aarni.rechardt@gmail.com>
2025-01-10 15:09:08 +0100

    add unit tests for timer.tsx

--- timerFront/__tests__/timers-test.tsx
+++ timerFront/__tests__/timers-test.tsx
@@ -22,10 +22,46 @@ describe('Timer', () => {
         expect(timer.getSecondsRemaining()).toBe(19)
     })
 
+    it("doesn't start timer automatically", () => {
+        addMockTime(5000)
+        expect(timer.getSecondsRemaining()).toBe(19)
+    })
+
     it('shows correct time after 5 seconds', () => {
         timer.pauseToggle()
         addMockTime(5000)
         expect(timer.getSecondsRemaining()).toBe(14)
+    })
+
+    it('starts timer after pauseToggle', () => {
+        timer.pauseToggle()
+        addMockTime(5000)
+        timer.pauseToggle()
+        addMockTime(5000)
+        expect(timer.getSecondsRemaining()).toBe(14)
+    })
+
+    it('swithces to break after active timer runs out', () => {
+        timer.pauseToggle()
+        addMockTime(20_001)
+        expect(timer.getSecondsRemaining()).toBe(4)
+        expect(timer.timerActive).toBeFalsy()
+    })
+
+    it('resets timer while in active state', () => {
+        timer.pauseToggle()
+        addMockTime(6000)
+        timer.resetTimer()
+        addMockTime(21_000)
+        expect(timer.getSecondsRemaining()).toBe(19)
+    })
+
+    it('resets timer while in break state', () => {
+        timer.pauseToggle()
+        addMockTime(21_000)
+        timer.resetTimer()
+        addMockTime(21_000)
+        expect(timer.getSecondsRemaining()).toBe(19)
     })
     
 })
