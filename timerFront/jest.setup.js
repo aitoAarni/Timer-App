2a385a84802cc2c0ab9c466688de795ebdf0028b
aitoAarni <aarni.rechardt@gmail.com>
2025-01-12 17:31:29 +0100

    change jest setup

--- timerFront/jest.setup.js
+++ timerFront/jest.setup.js
@@ -4,14 +4,14 @@ import 'react-native-gesture-handler/jestSetup'
 jest.mock('react-native-reanimated', () => {
     const Reanimated = jest.requireActual('react-native-reanimated/mock')
 
-    Reanimated.runOnJS = fn => fn
-    ;(Reanimated.clamp = (value, leftClamp, rightClamp) => {
+    Reanimated.clamp = (value, leftClamp, rightClamp) => {
         if (value > rightClamp) return rightClamp
         if (value < leftClamp) return leftClamp
         return value
-    }),
-        (Reanimated.withTiming = value => value)
-    Reanimated.withDelay = (delay, value) => value
+    }
+    // Reanimated.runOnJS = fn => fn
+    // (Reanimated.withTiming = value => value)
+    // Reanimated.withDelay = (delay, value) => value
 
     return Reanimated
 })
