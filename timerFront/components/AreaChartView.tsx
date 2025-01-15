9dc441925fc4bf85a883dc94228b8d19c6fb1ced
aitoAarni <aarni.rechardt@gmail.com>
2025-01-15 20:54:34 +0100

    style area chart

--- timerFront/components/AreaChartView.tsx
+++ timerFront/components/AreaChartView.tsx
@@ -1,35 +1,65 @@
+import theme from '@/theme'
 import { useState } from 'react'
+import { Dimensions } from 'react-native'
 import { LineChart } from 'react-native-gifted-charts'
 
 interface AreaChartViewProps {
     data: {
         value: number
         label?: string
+        endFillColor?: string
     }[]
 }
+
+const { width } = Dimensions.get('window')
 
 const AreaChartView = ({ data }: AreaChartViewProps) => {
     return (
         <LineChart
             areaChart
+            width={width}
+            stepValue={10}
             color="#33FFFF"
             animateOnDataChange
+            onDataChangeAnimationDuration={600}
             isAnimated
-            thickness1={5}
-            yAxisTextStyle={{ color: 'lightgray' }}
-            spacing={80}
-            backgroundColor="#414141"
             animationDuration={1.5}
-            startFillColor1="#43E4E9"
-            endFillColor1="#47E596"
-            endFillColor2="#3030f0"
-            startOpacity={0.9}
-            endOpacity1={0.4}
+            onPress={(item: { value: number }, index: number) => {
+                console.log(item, index)
+            }}
+            curved
+            curvature={0.1}
+            thickness1={8}
+            spacing={60}
+            startIndex1={0}
+            xAxisThickness={0}
+            yAxisThickness={0}
+            yAxisLabelWidth={0}
+            hideRules
+            xAxisLabelsHeight={0}
+            hideYAxisText
+            textColor1="white"
+            textFontSize1={16}
+            textShiftX={10}
+            textShiftY={-5}
+            yAxisTextStyle={{ color: 'lightgray' }}
+            backgroundColor={theme.colors.background}
+            startFillColor="green"
+            endFillColor1="blue"
+            startOpacity={0.5}
+            endOpacity={0.5}
             data={data}
             yAxisColor="lightgray"
             xAxisColor="lightgray"
             dataPointsHeight={20}
             dataPointsWidth={20}
+            dataPointsRadius={5}
+            dataPointsColor1="white"
+            gradientDirection="horizontal"
+            lineGradient
+            lineGradientDirection="horizontal"
+            lineGradientStartColor="green"
+            lineGradientEndColor="blue"
         />
     )
 }
--- timerFront/components/StatisticsView.tsx
+++ timerFront/components/StatisticsView.tsx
@@ -13,15 +13,33 @@ import { useDatabase } from '@/contexts/DatabaseContext'
 import { formatTotalTime } from '@/utils/format'
 import AreaChartView from './AreaChartView'
 const data1 = [
-    { value: 15, label: '15' },
-    { value: 30, label: '30' },
-    { value: 26 },
-    { value: 40 },
+    { value: 15, dataPointText: '15' },
+    { value: 30, dataPointText: '30' },
+    { value: 26, endFillColor: '#535353' },
+    { value: 40, endFillColor: '#528353' },
+    { value: 5, endFillColor: '#537233' },
+    { value: 25, endFillColor: '#526353' },
+    { value: 27, endFillColor: '#535246' },
+    { value: 65, endFillColor: '#525264' },
+    { value: 25 },
+    { value: 17 },
+    { value: 13 },
+    { value: 15 },
+    { value: 25 },
+    { value: 6 },
+    { value: 35 },
+    { value: 2 },
 ]
 const data2 = [
-    { value: 20, label: '20' },
-    { value: 10, label: '10' },
+    { value: 20, dataPointText: '20' },
+    { value: 10, dataPointText: '10' },
     { value: 6 },
+    { value: 15 },
+    { value: 15 },
+    { value: 15 },
+    { value: 15 },
+    { value: 15 },
+    { value: 15 },
     { value: 15 },
 ]
 
