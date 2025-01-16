2ebd1ab065cd56f9c05eae0e1b9e3e243922bb56
aitoAarni <aarni.rechardt@gmail.com>
2025-01-16 10:46:55 +0100

    add labels to show

--- timerFront/components/AreaChartView.tsx
+++ timerFront/components/AreaChartView.tsx
@@ -1,13 +1,17 @@
 import theme from '@/theme'
 import { useState } from 'react'
-import { Dimensions } from 'react-native'
+import { Dimensions, View } from 'react-native'
 import { LineChart } from 'react-native-gifted-charts'
+import Text from './Text'
 
 interface AreaChartViewProps {
     data: {
         value: number
         label?: string
         endFillColor?: string
+        labelComponent?: (val: string) => React.JSX.Element
+        secondaryLabel?: string
+        labelTextStyle?: { color: string }
     }[]
 }
 
@@ -15,52 +19,66 @@ const { width } = Dimensions.get('window')
 
 const AreaChartView = ({ data }: AreaChartViewProps) => {
     return (
-        <LineChart
-            areaChart
-            width={width}
-            stepValue={10}
-            color="#33FFFF"
-            animateOnDataChange
-            onDataChangeAnimationDuration={600}
-            isAnimated
-            animationDuration={1.5}
-            onPress={(item: { value: number }, index: number) => {
-                console.log(item, index)
-            }}
-            curved
-            curvature={0.1}
-            thickness1={8}
-            spacing={60}
-            startIndex1={0}
-            xAxisThickness={0}
-            yAxisThickness={0}
-            yAxisLabelWidth={0}
-            hideRules
-            xAxisLabelsHeight={0}
-            hideYAxisText
-            textColor1="white"
-            textFontSize={20}
-            textShiftX={10}
-            textShiftY={-5}
-            yAxisTextStyle={{ color: 'lightgray' }}
-            backgroundColor={theme.colors.background}
-            startFillColor="green"
-            endFillColor1="blue"
-            startOpacity={0.5}
-            endOpacity={0.5}
-            data={data}
-            yAxisColor="lightgray"
-            xAxisColor="lightgray"
-            dataPointsHeight={20}
-            dataPointsWidth={20}
-            dataPointsRadius={5}
-            dataPointsColor1="white"
-            gradientDirection="horizontal"
-            lineGradient
-            lineGradientDirection="horizontal"
-            lineGradientStartColor="green"
-            lineGradientEndColor="blue"
-        />
+        <View>
+            <LineChart
+                areaChart
+                width={width}
+                stepValue={10}
+                color="#33FFFF"
+                animateOnDataChange
+                onDataChangeAnimationDuration={600}
+                isAnimated
+                animationDuration={1.5}
+                onPress={(item: { value: number }, index: number) => {
+                    console.log(item, index)
+                }}
+                curved
+                curvature={0.1}
+                thickness1={8}
+                spacing={60}
+                startIndex1={0}
+                xAxisThickness={0}
+                yAxisThickness={0}
+                yAxisLabelWidth={0}  
+                hideRules
+                hideYAxisText
+                textColor1="white"
+                textFontSize={20}
+                textShiftX={10}
+                textShiftY={-5}
+                yAxisTextStyle={{ color: 'lightgray' }}
+                backgroundColor="gray" //{theme.colors.background}
+                startFillColor="green"
+                endFillColor1="blue"
+                startOpacity={0.5}
+                endOpacity={0.5}
+                data={data}
+                yAxisColor="lightgray"
+                xAxisColor="lightgray"
+                dataPointsHeight={20}
+                dataPointsWidth={20}
+                dataPointsRadius={7}
+                dataPointsColor1="white"
+                gradientDirection="horizontal"
+                lineGradient
+                lineGradientDirection="horizontal"
+                lineGradientStartColor="green"
+                lineGradientEndColor="blue"
+            />
+            <LineChart
+                data={data}
+                overflowBottom={10}
+                backgroundColor="white"
+            />
+        </View>
+    )
+}
+
+const CustomLabel = (val: string) => {
+    return (
+        <View style={{ width: 70, marginLeft: 7 }}>
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>{val}</Text>
+        </View>
     )
 }
 
--- timerFront/components/StatisticsView.tsx
+++ timerFront/components/StatisticsView.tsx
@@ -1,27 +1,69 @@
-import { Button, StyleSheet, View } from 'react-native'
-import Text from './Text'
+import { Button, StyleSheet, View, Text } from 'react-native'
 
 import { useEffect, useState } from 'react'
-import { getUsers } from '@/storage/local/userQueries'
-import {
-    getAllTimeData,
-    getAllTimes,
-    insertTime,
-} from '@/storage/local/timerQueries'
+import { getAllTimes } from '@/storage/local/timerQueries'
 import { TimeDuratio, TimeLogged } from '@/types'
 import { useDatabase } from '@/contexts/DatabaseContext'
 import { formatTotalTime } from '@/utils/format'
 import AreaChartView from './AreaChartView'
+
+const CustomLabel = (val: string) => {
+    return (
+        <View
+            style={{
+                flexGrow: 1,
+                backgroundColor: 'red',
+                alignItems: 'center',
+                justifyContent: 'center',
+            }}
+        >
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>{val}</Text>
+        </View>
+    )
+}
 const data1 = [
-    { value: 15, dataPointText: '15' },
-    { value: 30, dataPointText: '30' },
-    { value: 26, endFillColor: '#535353' },
-    { value: 40, endFillColor: '#528353' },
-    { value: 5, endFillColor: '#537233' },
+    {
+        value: 15,
+        dataPointText: '15',
+        secondaryLabel: 'jouuuu',
+        label: 'Nov 15',
+        labelTextStyle: { color: 'white' },
+    },
+    {
+        value: 30,
+        dataPointText: '30',
+        // labelComponent: () => CustomLabel('22 Nov'),
+
+        label: 'Nov 15',
+        labelTextStyle: { color: 'white' },
+    },
+    {
+        value: 26,
+        endFillColor: '#535353',
+        // labelComponent: () => CustomLabel('22 Nov'),
+
+        label: 'Nov 15',
+        labelTextStyle: { color: 'white' },
+    },
+    {
+        value: 40,
+        endFillColor: '#528353',
+        // labelComponent: () => CustomLabel('22 Nov'),
+
+        label: 'Nov 15',
+        labelTextStyle: { color: 'white' },
+    },
+    {
+        value: 5,
+        endFillColor: '#537233',
+        secondaryLabel: 'täääl',
+        dataPointText: '5',
+        labelComponent: () => CustomLabel('22 Nov'),
+    },
     { value: 25, endFillColor: '#526353' },
     { value: 27, endFillColor: '#535246' },
     { value: 65, endFillColor: '#525264' },
-    { value: 25 },
+    { value: 25, secondaryLabel: 'tääällää' },
     { value: 17 },
     { value: 13 },
     { value: 15 },
@@ -31,11 +73,11 @@ const data1 = [
     { value: 2 },
 ]
 const data2 = [
-    { value: 20, dataPointText: '20' },
+    { value: 20, dataPointText: '20', label: '15.3', secondaryLabel: 'ayoooo' },
     { value: 10, dataPointText: '10' },
-    { value: 6 },
+    { value: 6, labelComponent: () => CustomLabel('22 Nov') },
     { value: 15 },
-    { value: 15 },
+    { value: 15, labelComponent: () => CustomLabel('22 Nov') },
     { value: 15 },
     { value: 15 },
     { value: 15 },
@@ -46,7 +88,7 @@ const data2 = [
 export default function StatisticsView() {
     const [data, setData] = useState<string>('00:00')
     const [values, setValues] = useState(data1)
-    const [original, setOriginal] = useState(true)
+    const [original, setOriginal] = useState(false)
     const db = useDatabase()
     useEffect(() => {
         const getData = async () => {
