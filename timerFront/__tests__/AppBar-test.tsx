20f5b04ffe7770b1d414ed9be0441ffec643ce8c
aitoAarni <aarni.rechardt@gmail.com>
2025-01-20 13:32:22 +0100

    add unit testing for AppBar

---
+++ timerFront/__tests__/AppBar-test.tsx
@@ -0,0 +1,39 @@
+// @ts-nocheck
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react-native'
+import AppBar from '@/components/AppBar'
+import theme from '@/theme'
+
+jest.mock('expo-router', () => ({
+    Link: ({ children, href }: { children: React.ReactNode; href: string }) => (
+        <div data-testid={`link-${href}`}>{children}</div>
+    ),
+    usePathname: jest.fn(),
+}))
+
+describe('AppBar Component', () => {
+    it('renders all AppBarButton components', () => {
+        const { getByText } = render(
+            <AppBar
+                navigation={{}}
+                options={{}}
+                route={{}}
+                back={{ title: undefined, href: undefined }}
+            />
+        )
+
+        expect(getByText('Settings')).toBeTruthy()
+        expect(getByText('Timer')).toBeTruthy()
+        expect(getByText('Statistics')).toBeTruthy()
+    })
+
+    it('highlights the active link based on the current path', () => {
+        require('expo-router').usePathname.mockReturnValue('/settings')
+        const { getByText } = render(
+            <AppBar navigation={{}} options={{}} back={false} />
+        )
+
+        const activeText = getByText('Settings')
+        expect(activeText).toHaveStyle({ color: theme.colors.text })
+    })
+})
