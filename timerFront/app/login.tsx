fa76b6195c2e39b61a951b41875b914efbd5e7ac
aitoAarni <aarni.rechardt@gmail.com>
2025-01-26 21:44:41 +0100

    add error handling to SignInView

--- timerFront/app/login.tsx
+++ timerFront/app/login.tsx
@@ -1,5 +1,7 @@
+import ErrorBox from '@/components/ErrorBox'
 import SignInView from '@/components/SignInView'
 import theme from '@/theme'
+import { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 
 export default function LoginPage() {
--- timerFront/components/SignInView.tsx
+++ timerFront/components/SignInView.tsx
@@ -1,7 +1,6 @@
 import {
     TextInput,
     View,
-    Button,
     StyleSheet,
     Dimensions,
     TouchableOpacity,
@@ -9,11 +8,37 @@ import {
 import { useForm, SubmitHandler, Controller } from 'react-hook-form'
 import Text from './Text'
 import theme from '@/theme'
+import ErrorBox from './ErrorBox'
+import { useEffect, useState } from 'react'
 
 interface Inputs {
     username: string
     password: string
-    email?: string
+}
+
+const rules = {
+    username: {
+        required: 'Username is required',
+        minLength: {
+            value: 3,
+            message: 'Username must be at least 3 characters long',
+        },
+        maxLength: {
+            value: 20,
+            message: 'Username cannot exceed 20 characters',
+        },
+    },
+    password: {
+        required: 'Password is required',
+        minLength: {
+            value: 8,
+            message: 'Password must be at least 8 characters long',
+        },
+        maxLength: {
+            value: 25,
+            message: 'Password cannot exceed 25 characters',
+        },
+    },
 }
 
 const width = Dimensions.get('window').width
@@ -24,21 +49,34 @@ export default function SignInView() {
         handleSubmit,
         control,
         formState: { errors },
-    } = useForm<Inputs>()
-
+    } = useForm<Inputs>({
+        defaultValues: {
+            username: '',
+            password: '',
+        },
+    })
+    const [errorMessage, setErrorMessage] = useState<string | null>(null)
+    useEffect(() => {
+        setErrorMessage('errorii nääs')
+    }, [])
     const onSubmit: SubmitHandler<Inputs> = data => {
         console.log(data)
     }
-
     return (
         <View style={styles.container}>
+            <ErrorBox
+                errorMessage={errorMessage}
+                setErrorMessage={setErrorMessage}
+            />
             <Text style={styles.header}>Create an account</Text>
             <Text style={styles.text}>Username:</Text>
             <Controller
                 control={control}
+                rules={rules.username}
                 name="username"
                 render={({ field: { onChange, onBlur, value } }) => (
                     <TextInput
+                        maxLength={20}
                         placeholderTextColor={theme.colors.grayLight}
                         style={styles.input}
                         onChangeText={onChange}
@@ -47,12 +85,18 @@ export default function SignInView() {
                     />
                 )}
             />
+            <Text fontSize={12} color={theme.colors.error}>
+                {errors.username?.message ?? ''}
+            </Text>
             <Text style={styles.text}>Password:</Text>
             <Controller
                 control={control}
+                rules={rules.password}
                 name="password"
                 render={({ field: { onChange, onBlur, value } }) => (
                     <TextInput
+                        secureTextEntry
+                        maxLength={25}
                         placeholderTextColor={theme.colors.grayLight}
                         style={styles.input}
                         onChangeText={onChange}
@@ -61,20 +105,12 @@ export default function SignInView() {
                     />
                 )}
             />
-            <Text style={styles.text}>Email:</Text>
-            <Controller
-                control={control}
-                name="email"
-                render={({ field: { onChange, onBlur, value } }) => (
-                    <TextInput
-                        placeholderTextColor={theme.colors.grayLight}
-                        style={styles.input}
-                        onChangeText={onChange}
-                        value={value}
-                        placeholder="email"
-                    />
-                )}
-            />
+            {errors.password && (
+                <Text fontSize={12} color={theme.colors.error}>
+                    {errors.password.message}
+                </Text>
+            )}
+
             <TouchableOpacity
                 style={styles.button}
                 onPress={handleSubmit(onSubmit)}
@@ -102,7 +138,6 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         paddingHorizontal: 15,
         paddingVertical: 5,
-        marginBottom: 15,
     },
     text: { fontSize: 20, color: theme.colors.grayLight },
     button: { width: '100%', color: theme.colors.grayLight, fontSize: 30 },
