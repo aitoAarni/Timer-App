48c902d597f35dbff0f84bc33e564b30a77a9cb7
aitoAarni <aarni.rechardt@gmail.com>
2025-01-27 20:56:15 +0100

    create authStorage class

---
+++ timerFront/utils/authStorage.ts
@@ -0,0 +1,28 @@
+import {
+    getSecureKeyValuePair,
+    setSecureKeyValuePair,
+} from '@/storage/local/secureKeyValueStorage'
+import { User } from '@/types'
+
+export default class authStorage {
+    key: string
+    constructor() {
+        this.key = 'authUser'
+    }
+    async getUser() {
+        try {
+            const user = await getSecureKeyValuePair(this.key)
+            return user
+        } catch (error) {
+            throw error
+        }
+    }
+    async setUser(user: User) {
+        const userString = JSON.stringify(user)
+        try {
+            await setSecureKeyValuePair(this.key, userString)
+        } catch (error) {
+            throw error
+        }
+    }
+}
