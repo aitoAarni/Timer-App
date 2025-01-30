fbffd653468a7b6dd0350e13cc6fe7bf6a3de28d
aitoAarni <aarni.rechardt@gmail.com>
2025-01-30 14:42:00 +0100

    create swipeNavigation component

--- timerFront/components/SwipeNavigation.tsx
+++ timerFront/components/SwipeNavigation.tsx
@@ -1,11 +1,19 @@
 import { ReactNode, useState } from 'react'
-import { StyleSheet, View } from 'react-native'
+import {
+    GestureResponderEvent,
+    StyleProp,
+    StyleSheet,
+    View,
+    ViewStyle,
+} from 'react-native'
 
 interface SwipeNavigationProps {
     registerSwipe: boolean
     rightSwipeCallback: () => void | null
     leftSwipeCallback: () => void | null
     children: ReactNode
+    swipeDelta: number
+    style: StyleProp<ViewStyle>
 }
 
 export default function SwipeNavigation({
@@ -13,13 +21,35 @@ export default function SwipeNavigation({
     rightSwipeCallback,
     leftSwipeCallback,
     children,
+    swipeDelta = 50,
+    style,
 }: SwipeNavigationProps) {
     const [touchStartX, setTouchStartX] = useState(0)
-    const [touchEndX, setTouchEndX] = useState(0)
 
-    
+    const onTouchStart = (event: GestureResponderEvent) => {
+        const startX = event.nativeEvent.pageX
+        setTouchStartX(startX)
+    }
+    const onTouchEnd = (event: GestureResponderEvent) => {
+        if (!registerSwipe) return
+        const endX = event.nativeEvent.pageX
+        const currentSwipeDelta = endX - touchStartX
+        console.log('swipe: ', currentSwipeDelta)
+        if (currentSwipeDelta > swipeDelta) {
+            leftSwipeCallback()
+            return
+        }
+        if (currentSwipeDelta < swipeDelta) {
+            rightSwipeCallback()
+            return
+        }
+    }
 
-    return <View style={styles.container}>{children}</View>
+    return (
+        <View onResponderStart={onTouchStart} style={[styles.container, style]}>
+            {children}
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({ container: { flex: 1 } })
