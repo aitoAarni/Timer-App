50b71aafd23bd539e095f449dc28d9f06f44acb5
aitoAarni <aarni.rechardt@gmail.com>
2025-01-31 15:49:48 +0100

    create profile components

---
+++ timerFront/components/ProfileView.tsx
@@ -0,0 +1,10 @@
+import SwipeNavigation from './SwipeNavigation'
+import Text from './Text'
+
+export default function ProfileView() {
+    return (
+        <SwipeNavigation>
+            <Text>profile view</Text>
+        </SwipeNavigation>
+    )
+}
