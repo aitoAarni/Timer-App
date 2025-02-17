a1cf9f9fd85676aacc97710e6a77f0749373536a
aitoAarni <aarni.rechardt@gmail.com>
2025-02-17 18:41:46 +0200

    create type guard for user credentials

--- backEnd/src/utils.ts
+++ backEnd/src/utils.ts
@@ -1,15 +1,31 @@
 import { UserCredentials } from './types'
 
 export const toUserCredentials = (object: unknown): UserCredentials => {
-    console.log(object)
-    const userCredentials = {
-        username: 'sped',
-        password: 'pasanen',
+    if (!object || typeof object !== 'object') {
+        throw new Error('Incorrect or missing data')
     }
-    return userCredentials
+    if ('username' in object && 'password' in object) {
+        const userCredentials = {
+            username: parseUsername(object.username),
+            password: parsePassword(object.password),
+        }
+        return userCredentials
+    }
+    throw new Error('Incorrect data: some fields are missing')
 }
 
-
+const parseUsername = (username: unknown): string => {
+    if (username && isString(username)) {
+        return username
+    }
+    throw new Error('Incorrect or missing usernamee')
+}
+const parsePassword = (password: unknown): string => {
+    if (password && isString(password)) {
+        return password
+    }
+    throw new Error('Incorrect or missing password')
+}
 
 const isString = (text: unknown): text is string => {
     return typeof text === 'string' || text instanceof String
