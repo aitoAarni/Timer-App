4dbbfd4440996e660c6f83bb5b4b332bad337d25
aitoAarni <aarni.rechardt@gmail.com>
2025-02-19 17:51:56 +0200

    create config file

---
+++ backEnd/src/config.ts
@@ -0,0 +1,17 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const getEnvVar = (key: string): string => {
+    const value = process.env[key]
+    if (!value) {
+        throw new Error(`Missing environment variable: ${key}`)
+    }
+    return value
+}
+export const SALT_ROUNDS = getEnvVar('SALT_ROUNDS')
+
+export const SECRET = getEnvVar('SECRET')
+export const DATABASE_URI = getEnvVar(
+    process.env.NODE_ENV === 'test' ? 'TEST_MONGODB_URI' : 'MONGODB_URI'
+)
