4479a699b2c5f41d21116c68d2afe51c6712b262
aitoAarni <aarni.rechardt@gmail.com>
2025-02-19 18:50:07 +0200

    refactor app structure for tests

---
+++ backEnd/src/app.ts
@@ -0,0 +1,36 @@
+import {DATABASE_URI} from './config'
+import express from 'express'
+import userRouter from './routes/userRouter'
+import mongoose from 'mongoose'
+import { errorHandler, unknownEndpoint } from './middleware'
+
+if (!DATABASE_URI) {
+    throw new Error('MONGODB_URI environmental variable is not set')
+}
+
+mongoose
+    .connect(DATABASE_URI)
+    .then(() => console.log('connected to db'))
+    .catch(error => {
+        console.error(
+            'error connecting to db',
+            error instanceof Error ? error.message : String(error)
+        )
+    })
+
+const app = express()
+
+app.use(express.json())
+
+
+app.get('/ping', (_req, res) => {
+    console.log('someone pinged here')
+    res.send('pong')
+})
+
+app.use('/api/user', userRouter)
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
+
+export default app
--- backEnd/src/index.ts
+++ backEnd/src/index.ts
@@ -1,38 +1,7 @@
-import {DATABASE_URI} from './config'
-import express from 'express'
-import userRouter from './routes/userRouter'
-import mongoose from 'mongoose'
-import { errorHandler, unknownEndpoint } from './middleware'
+import app from "./app"
 
-if (!DATABASE_URI) {
-    throw new Error('MONGODB_URI environmental variable is not set')
-}
-
-mongoose
-    .connect(DATABASE_URI)
-    .then(() => console.log('connected to db'))
-    .catch(error => {
-        console.error(
-            'error connecting to db',
-            error instanceof Error ? error.message : String(error)
-        )
-    })
-
-const app = express()
-
-app.use(express.json())
 
 const PORT = 3000
-
-app.get('/ping', (_req, res) => {
-    console.log('someone pinged here')
-    res.send('pong')
-})
-
-app.use('/api/user', userRouter)
-
-app.use(unknownEndpoint)
-app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server running on port: ${PORT}`)
