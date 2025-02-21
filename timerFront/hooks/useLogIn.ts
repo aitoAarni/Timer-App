72d16af8d8f1963dc6ba19aeac9e5e9c4f27e406
aitoAarni <aarni.rechardt@gmail.com>
2025-02-21 23:38:18 +0200

    add all remote logging in functionality

--- timerFront/hooks/useLogIn.ts
+++ timerFront/hooks/useLogIn.ts
@@ -2,23 +2,40 @@ import { getUserByUsername } from '@/storage/local/userQueries'
 import AuthStorage from '@/utils/authStorage'
 import { useDispatch } from 'react-redux'
 import { setLoggedInUser } from '@/redux/userSlice'
-import login from '@/services/login'
+import login from '@/services/loginServices'
 import { RemoteUser } from '@/types'
-import createLocalUser from '@/services/userServices'
+import { createLocalUser, createRemoteUser } from '@/services/userServices'
 
 const useLogIn = () => {
     const dispatch = useDispatch()
     const logIn = async (username: string, password: string) => {
+        let remoteUser: RemoteUser | null = null
+        let requestNotPossible: boolean
         try {
-            let remoteUser
-            try {
-                remoteUser = await login(username, password)
-            } catch {
-                remoteUser = null
-            }
-
-            const localUser = (await getUserByUsername(username))[0]
+            remoteUser = await login(username, password)
+            requestNotPossible = false
+        } catch (error) {
+            console.error(error)
+            requestNotPossible = true
+        }
+        console.log(
+            'remoteUser: ',
+            remoteUser,
+            '  requestNotPossible',
+            requestNotPossible
+        )
+        try {
+            let localUser = (await getUserByUsername(username))[0]
             if (localUser && localUser.password === password) {
+                console.log('localUser && localUser.password === password')
+                if (!requestNotPossible && !remoteUser) {
+                    console.log('!requestNotPossible && !remoteUser')
+                    const newUser = await createRemoteUser(username, password)
+                    console.log('new user: ', newUser)
+                    console.log('trying to log in again')
+                    remoteUser = await login(username, password)
+                    console.log('remoteUser now: ', remoteUser)
+                }
                 const storageUser = {
                     ...localUser,
                     token: remoteUser ? remoteUser.token : null,
@@ -28,11 +45,13 @@ const useLogIn = () => {
                 dispatch(setLoggedInUser(storageUser))
                 return true
             } else if (remoteUser) {
+                console.log('else if (remoteUser)')
+                await createLocalUser(username, password, remoteUser.id)
+                localUser = (await getUserByUsername(username))[0]
                 const storageUser = {
                     ...localUser,
                     token: remoteUser ? remoteUser.token : null,
                 }
-                await createLocalUser(username, undefined, remoteUser.id)
                 const authStorage = new AuthStorage()
                 await authStorage.setUser(storageUser)
                 dispatch(setLoggedInUser(storageUser))
