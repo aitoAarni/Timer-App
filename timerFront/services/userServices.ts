2e2cf96c826b6d8284d212d9a09a27636d142b5d
aitoAarni <aarni.rechardt@gmail.com>
2025-02-21 23:36:57 +0200

    add remote user creation service

--- timerFront/services/userServices.ts
+++ timerFront/services/userServices.ts
@@ -1,6 +1,6 @@
 import { insertUser } from '@/storage/local/userQueries'
 
-export default async function createLocalUser(
+export async function createLocalUser(
     username: string,
     password: string | undefined,
     server_id: string | undefined = undefined
@@ -9,6 +9,32 @@ export default async function createLocalUser(
         await insertUser(username, password, server_id)
     } catch (error) {
         console.error(error)
+        throw error
+    }
+}
+
+export async function createRemoteUser(username: String, password: string) {
+    const body = JSON.stringify({ username, password })
+    try {
+        const response = await fetch(
+            'http://192.168.1.120:3000/api/user/create',
+            {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body,
+            }
+        )
+        if (!response.ok) {
+            const errorText = await response.text()
+            console.error(`Http: ${response.status}, ${errorText}`)
+            return null
+        }
+        const api = await response.json()
+        return api
+    } catch (error) {
+        console.error(error instanceof Error ? error.message : String(error))
         throw error
     }
 }
