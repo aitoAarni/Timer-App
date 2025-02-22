07dc39f89404dfa78d78addca33de3172a77d163
aitoAarni <aarni.rechardt@gmail.com>
2025-02-22 20:18:58 +0200

    fix make jest.config.js to CommonJS

--- timerFront/jest.config.js
+++ timerFront/jest.config.js
@@ -1,22 +1,24 @@
-export const preset = 'jest-expo';
-export const setupFiles = [
-    './node_modules/react-native-gesture-handler/jestSetup.js',
-    './jest.setup.js'
-];
-export const moduleNameMapper = {
-    '^@/(.*)$': '<rootDir>/$1'
-};
-export const collectCoverage = true;
-export const collectCoverageFrom = [
-    'components/**/*.tsx',
-    'hooks/**/*.ts',
-    'services/**/*.ts',
-    'utils/**/*.ts*',
-    'contexts/**/*.tsx',
-    'core/**/*.ts*',
-    'redux/**/*.ts',
-    'storage/**/*.ts',
-    '!**/node_modules/**',
-    '!**/vendor/**'
-];
-export const coverageReporters = ['json', 'lcov', 'text', 'clover'];
+module.exports = {
+    preset: 'jest-expo',
+    setupFiles: [
+        './node_modules/react-native-gesture-handler/jestSetup.js',
+        './jest.setup.js',
+    ],
+    moduleNameMapper: {
+        '^@/(.*)$': '<rootDir>/$1',
+    },
+    collectCoverage: true,
+    collectCoverageFrom: [
+        'components/**/*.tsx',
+        'hooks/**/*.ts',
+        'services/**/*.ts',
+        'utils/**/*.ts*',
+        'contexts/**/*.tsx',
+        'core/**/*.ts*',
+        'redux/**/*.ts',
+        'storage/**/*.ts',
+        '!**/node_modules/**',
+        '!**/vendor/**',
+    ],
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+}
