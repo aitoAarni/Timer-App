860cfb16c9aee832022737401d2f0276f0624ad8
aitoAarni <aarni.rechardt@gmail.com>
2025-02-24 16:01:09 +0200

    rename file

--- backEnd/src/routes/timeLogRouter.ts
+++ backEnd/src/routes/timeLogRouter.ts
@@ -1,12 +1,20 @@
 import express from 'express'
 import { toTimeLog } from '../utils'
 import TimeLog from '../models/timeLogModel'
+import authMiddleware, { AuthRequest } from '../middleware'
 
 const timerRouter = express.Router()
 
-timerRouter.post('/:UserId', async (req, res, next) => {
+timerRouter.post('/', authMiddleware, async (req: AuthRequest, res, next) => {
     try {
+        req.user = req.user as { username: string; id: string }
+        console.log('in timeLogRouter req.body: ', req.body)
+        console.log('req.user: ', req.user)
         const { created_at, duration, user_id } = toTimeLog(req.body)
+
+        if (!(req.user.id !== user_id)) {
+            throw new Error('Time log must be created by authenticated user')
+        }
         const time = new TimeLog({ created_at, duration, user_id })
         const savedTimeLog = await time.save()
         const savedTimeLogJson = savedTimeLog.toJSON()
