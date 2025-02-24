39ee8085c14ad4d9bb6b404ab0382aba387501f5
aitoAarni <aarni.rechardt@gmail.com>
2025-02-24 14:33:27 +0200

    create TimeLog parsing

--- backEnd/src/utils.ts
+++ backEnd/src/utils.ts
@@ -1,4 +1,4 @@
-import { UserCredentials } from './types'
+import { TimeLog, UserCredentials } from './types'
 import { z } from 'zod'
 
 export const UserCredentialsSchema = z.object({
@@ -7,4 +7,14 @@ export const UserCredentialsSchema = z.object({
 })
 export const toUserCredentials = (object: unknown): UserCredentials => {
     return UserCredentialsSchema.parse(object)
+}
+
+export const TimeLogSchema = z.object({
+    created_at: z.string(),
+    duration: z.number(),
+    user_id: z.string(),
+})
+
+export const toTimeLog = (object: unknown): TimeLog => {
+    return TimeLogSchema.parse(object)
 }
