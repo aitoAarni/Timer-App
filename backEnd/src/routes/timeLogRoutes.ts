f52cde1f9a28029c0e4197e285d173f214f46214
aitoAarni <aarni.rechardt@gmail.com>
2025-02-24 14:35:35 +0200

    update timeLogRoute to connect to db

---
+++ backEnd/src/routes/timeLogRoutes.ts
@@ -0,0 +1,20 @@
+import express from 'express'
+import { toTimeLog } from '../utils'
+import TimeLog from '../models/timeLogModel'
+
+const timerRouter = express.Router()
+
+timerRouter.post('/:UserId', async (req, res, next) => {
+    try {
+        const { created_at, duration, user_id } = toTimeLog(req.body)
+        const time = new TimeLog({ created_at, duration, user_id })
+        const savedTimeLog = await time.save()
+        const savedTimeLogJson =  savedTimeLog.toJSON()
+        res.status(200).send(savedTimeLogJson)
+
+    } catch (error) {
+        next(error)
+    }
+})
+
+export default timerRouter
