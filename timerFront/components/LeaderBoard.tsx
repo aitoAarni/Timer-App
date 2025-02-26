f62e7b20cfb796ba8b1d6299c6f72a76e8b6960c
aitoAarni <aarni.rechardt@gmail.com>
2025-02-26 23:40:22 +0200

    switch to real data on leaderboard

--- timerFront/components/LeaderBoard.tsx
+++ timerFront/components/LeaderBoard.tsx
@@ -1,34 +1,59 @@
-import { FlatList, StyleSheet, TouchableOpacity, View } from 'react-native'
+import {
+    ActivityIndicator,
+    FlatList,
+    StyleSheet,
+    TouchableOpacity,
+    View,
+} from 'react-native'
 import Text from './Text'
 import { formatTime } from '@/utils/format'
 import theme from '@/theme'
+import { getRankings } from '@/services/rankingServices'
+import { useEffect, useRef, useState } from 'react'
 
 interface LeaderBoardProps {
     userId: string | null
 }
 
 export default function LeaderBoard({ userId }: LeaderBoardProps) {
-    const rankPercentage = Math.round(
-        (data.userRank / data.totalParticipants) * 100
+    if (!userId) return
+    const [rankings, setRankings] = useState<Rankings | null>(null)
+
+    const rankPercentage = rankings && Math.round(
+        (rankings.userRank / rankings.totalParticipants) * 100
     )
-    const refreshData = () => {}
-    return (
+    useEffect(() => {
+            console.log('fetching rankings')
+            refreshData()
+        
+    }, [])
+    const refreshData = async () => {
+        const fetchedRankings = await getRankings(userId, '2025-02-26')
+        setRankings(fetchedRankings)
+        console.log(rankings)
+    }
+
+    return rankings ? (
         <View style={styles.container}>
             <Text style={styles.headerText}>
-                Your rank ({data.userRank}/{data.totalParticipants}) top{' '}
+                Your rank ({rankings.userRank}/{rankings.totalParticipants}) top{' '}
                 {rankPercentage}%
             </Text>
             <TouchableOpacity onPress={refreshData}>
                 <Text fontSize={29}>refresh</Text>
             </TouchableOpacity>
-            <FlatList
-                data={data.nearbyUsers}
-                renderItem={({ item }) => (
-                    <Item item={item} currentUserId={userId} />
-                )}
-                keyExtractor={item => item.user_id}
-            />
+            {rankings && (
+                <FlatList
+                    data={rankings.nearbyUsers}
+                    renderItem={({ item }) => (
+                        <Item item={item} currentUserId={userId} />
+                    )}
+                    keyExtractor={item => item.user_id}
+                />
+            )}
         </View>
+    ) : (
+        <ActivityIndicator style={styles.container} />
     )
 }
 
@@ -109,52 +134,17 @@ const styles = StyleSheet.create({
     itemText: { fontSize: 20, flex: 1, color: theme.colors.text },
 })
 
-const data = {
-    userRank: 4,
-    userDuration: 5632,
-    nearbyUsers: [
-        {
-            rank: 1,
-            user_id: '67bf056ba68f3fc0c9f952c4',
-            username: 'username',
-            duration: 10056,
-        },
-        {
-            rank: 2,
-            user_id: '67bf05d3a68f3fc0c9f952cf',
-            username: 'Lil killah',
-            duration: 7340,
-        },
-        {
-            rank: 3,
-            user_id: '67b628d7c22cdf7238ba76aa',
-            username: 'bro',
-            duration: 5632,
-        },
-        {
-            rank: 4,
-            user_id: '67bf09c814b56599a177c9e9',
-            username: 'bruh boi',
-            duration: 2253,
-        },
-        {
-            rank: 5,
-            user_id: 'e67b628d7c22cdf7238ba76aa',
-            username: 'bro5',
-            duration: 632,
-        },
-        {
-            rank: 6,
-            user_id: '667b628d7c22cdf7238ba76aa',
-            username: 'brosef',
-            duration: 622,
-        },
-        {
-            rank: 7,
-            user_id: '67eb628d7c22cdf7238ba76aa',
-            username: 'brosefssor',
-            duration: 32,
-        },
-    ],
-    totalParticipants: 7,
+interface NearbyUser {
+    rank: number
+    user_id: string
+    username: string
+    duration: number
 }
+
+interface Rankings {
+    userRank: number
+    userDuration: number
+    totalParticipants: number
+    nearbyUsers: NearbyUser[]
+}
+
