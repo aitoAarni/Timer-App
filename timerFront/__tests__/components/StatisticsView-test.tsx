5bfbef4e02d3d542a1ea7b91e5f7ec3e2f35bebb
aitoAarni <aarni.rechardt@gmail.com>
2025-03-02 16:16:39 +0200

    create statistics view unit tests

---
+++ timerFront/__tests__/components/StatisticsView-test.tsx
@@ -0,0 +1,77 @@
+// @ts-nocheck
+import {
+    render,
+    screen,
+    fireEvent,
+    waitFor,
+} from '@testing-library/react-native'
+import StatisticsView from '@/components/StatisticsView'
+import { useSelector } from 'react-redux'
+import { getTimesGroupedByDate } from '@/storage/local/timerQueries'
+import { transformDatesAndDurationDataForChart } from '@/utils/dataHandlers'
+import { act } from 'react-test-renderer'
+import { Text } from 'react-native'
+
+jest.mock('react-redux', () => ({
+    useSelector: jest.fn(),
+}))
+
+jest.mock('@/hooks/useNavigateTo', () => () => jest.fn())
+jest.mock('@/storage/local/timerQueries', () => ({
+    getTimesGroupedByDate: jest.fn().mockReturnValue([]),
+}))
+jest.mock('@/utils/dataHandlers', () => ({
+    transformDatesAndDurationDataForChart: jest.fn().mockReturnValue([], 0),
+}))
+jest.mock('@/components/AreaChartView', () => () => {
+    const { View } = require('react-native')
+    return <View testID="area-chart-view"></View>
+})
+jest.mock('@/components/LeaderBoard', () => () => {
+    const { View } = require('react-native')
+    return <View testID="leaderboard"></View>
+})
+jest.mock('@/components/ErrorBox', () => ({ errorMessage }) => {
+    const { Text } = require('react-native')
+    return errorMessage ? <Text>{errorMessage}</Text> : null
+})
+
+const mockUser = { id: 'user123', server_id: 'server123' }
+
+beforeEach(() => {
+    jest.clearAllMocks()
+})
+
+describe('StatisticsView', () => {
+    it('renders login prompt if user is not logged in', () => {
+        useSelector.mockReturnValue(null)
+        render(<StatisticsView />)
+        expect(
+            screen.getByText('You must be logged in to get statistics')
+        ).toBeTruthy()
+    })
+
+    it('renders components if user is logged in', async () => {
+        const mockUser = {
+            id: 1,
+            username: 'testUser',
+            password: 'correctPassword',
+            server_id: 'serverid',
+            created_at: '2025-02-28',
+            token: 'Bearer token',
+        }
+        const mockData = [{ date: '2025-02-28', duration: 30 }]
+        const mockTransformedData = { transformedData: mockData, maxValue: 100 }
+        getTimesGroupedByDate.mockResolvedValue(mockData)
+        transformDatesAndDurationDataForChart.mockReturnValue(
+            mockTransformedData
+        )
+
+        useSelector.mockReturnValue(mockUser)
+        render(<StatisticsView />)
+        await waitFor(() => {
+            expect(screen.getByTestId('area-chart-view')).toBeTruthy()
+            expect(screen.getByTestId('leaderboard')).toBeTruthy()
+        })
+    })
+})
