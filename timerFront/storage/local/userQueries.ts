52c7b351468b3fa2a1f741286aaa09975dae89ed
aitoAarni <aarni.rechardt@gmail.com>
2025-03-09 13:25:49 +0200

    remove unused imports

--- timerFront/storage/local/userQueries.ts
+++ timerFront/storage/local/userQueries.ts
@@ -1,6 +1,3 @@
-import { User } from '@/types'
-import * as sqlite from 'expo-sqlite'
-import { openDatabase } from './db'
 
 export const insertUserQuery = `INSERT INTO users (username, password, server_id) VALUES (?, ?, ?)`
 
