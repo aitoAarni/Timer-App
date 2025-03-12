4ab2d2dd5192945cbfb0fd1b042db4db649bbac9
aitoAarni <aarni.rechardt@gmail.com>
2025-03-12 16:15:41 +0200

    create and fix e2e tests

--- timerFront/storage/local/initializeStorage.ts
+++ timerFront/storage/local/initializeStorage.ts
@@ -1,20 +1,38 @@
 import { clearSettings, getSettings } from '@/services/settingServices'
 import { createTables, initializeDatabase } from './db'
-import { isTest } from '@/utils/environment'
+import { BACK_END_URL, isTest } from '@/utils/environment'
 import { clearUser } from '@/redux/userSlice'
 import AuthStorage from '@/services/authStorageServices'
 import store from '@/redux/store'
 import { updateSettings } from '@/redux/settingsSlice'
-import { createLocalUser } from '@/services/userServices'
+import createUser, { createLocalUser } from '@/services/userServices'
 
 export default async function initializeStorage() {
     await initializeDatabase()
-
     await createTables()
 
     if (isTest()) {
-        await createLocalUser('test_user', 'password', null)
-
+        await fetch(`${BACK_END_URL}/api/user/remove`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: 'Tech bro',
+                password: 'secret password',
+            }),
+        })
+        await fetch(`${BACK_END_URL}/api/user/remove`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                username: 'test_user',
+                password: 'password',
+            }),
+        })
+        await createUser('test_user', 'password')
         await clearSettings()
         const updatedSettings = await getSettings()
         store.dispatch(updateSettings(updatedSettings))
